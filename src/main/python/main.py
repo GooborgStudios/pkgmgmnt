66a4a57d6a4c4d96575eaffed723b0698f12bc8e
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-05-15 16:52:36 -0700

    Standardize main.py

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,16 @@
-from fbs_runtime.application_context import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+# -*- coding: utf-8 -*-
+
+# pkgmgmnt: main.py
+# Copyright (c) 2019, Gooborg Studios.  All rights reserved.
 
 import sys
 
-if __name__ == '__main__':
-	appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-	window = QMainWindow()
-	window.resize(250, 150)
-	window.show()
-	exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-	sys.exit(exit_code)
+from fbs_runtime.application_context import ApplicationContext
+from PyQt5.QtWidgets import QMainWindow
+
+appctxt = ApplicationContext()
+window = QMainWindow()
+window.resize(250, 150)
+window.show()
+exit_code = appctxt.app.exec_()
+sys.exit(exit_code)
