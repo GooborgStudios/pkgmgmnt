cbf4d9ea31c0aa8a35770f77060002e52f0ccba2
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-05-22 17:19:06 -0700

    Allow additional arguments in Config.create_data_dir()

--- src/main/python/config.py
+++ src/main/python/config.py
@@ -18,12 +18,14 @@ class Config():
 
 		self.create_data_dir()
 
-	def create_data_dir(self):
-		if os.path.exists(self.DATA_PATH):
-			return # Directory already created
-
+	def create_data_dir(self, *args):
 		if not os.path.exists(self.GOOBORG_DATA_PATH):
 			os.mkdir(self.GOOBORG_DATA_PATH)
 			# XXX Create little readme.txt here later on
 
-		os.mkdir(self.DATA_PATH)
+		if not os.path.exists(self.DATA_PATH):
+			os.mkdir(self.DATA_PATH)
+
+		full_path = os.path.join(self.DATA_PATH, *args)
+		if not os.path.exists(full_path):
+			os.makedirs(full_path)
