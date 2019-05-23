7488a96cc68d7dcb75d69e80cec0488230979f35
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-05-22 17:12:22 -0700

    Implement Homebrew installation check

--- src/main/python/pkgmanagers/homebrew.py
+++ src/main/python/pkgmanagers/homebrew.py
@@ -3,16 +3,42 @@
 # pkgmgmnt: pkgmanagers/homebrew.py
 # Copyright (c) 2019, Gooborg Studios.  All rights reserved.
 
+import subprocess
+
+from config import Config
+
 from .main import *
 
 class Homebrew(PackageManager):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, app_config):
+		super().__init__(app_config)
 		self.name = "Homebrew"
+
+		try:
+			brew_config = subprocess.run(['brew', 'config'], capture_output=True)
+			config_data = brew_config.stdout.split(b'\n')
+			for c in config_data:
+				try:
+					k, v = c.decode('utf-8', 'ignore').split(": ", 1)
+				except ValueError:
+					continue
+
+				if k == 'HOMEBREW_VERSION':
+					self.version = v
+				if k == 'Core tap HEAD':
+					self.core_tap_head = v
+		except (FileNotFoundError, IndexError): # Homebrew isn't installed
+			self.version = None
+			self.core_tap_head = None
+
+		print("Homebrew Version: {0}".format(self.version))
+		print("Core Tap Head: {0}".format(self.core_tap_head))
 
 		self.update()
 
 	def update(self):
+		# package_info = subprocess.run(['brew', 'info', '--all', '--json'], capture_output=True)
+
 		# XXX This is pseudo-data for testing purposes.  Provide real data later.
 		
 		self._addpackage(Package(self, 'python', '3.7.3', '3.7.3'))
